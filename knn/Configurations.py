e8631f0b8027ea737c4ffe922bd422826f2eb305
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-02-15 19:44:44 +0800

    Updates to Python files

--- knn/Configurations.py
+++ knn/Configurations.py
@@ -1,5 +1,5 @@
 # input file names without extensions (all CSV)
-file_names = ["charities_with_ipc", "charities_financial_information", "charities_registered", "charities_with_tax_exempt"]
+file_names = ["charities_with_ipc", "charities_financial_information"]
 
 # output file name
 output_file_name = "combined_charities.csv"
---
+++ knn/KNNExecutor.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+
+class KNNExecutor:
+
+    def performKNN(self, df, k):
